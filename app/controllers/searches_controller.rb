4b46925bd503055ef017ab098cef68eb9192af3a
captproton <carl@wdwhub.net>
2017-10-01 12:49:46 -0700

    INIT: search scaffold

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,58 @@
+class SearchesController < ApplicationController
+  before_action :set_search, only: [:show, :edit, :update, :destroy]
+
+  # GET /searches
+  def index
+    @searches = Search.all
+  end
+
+  # GET /searches/1
+  def show
+  end
+
+  # GET /searches/new
+  def new
+    @search = Search.new
+  end
+
+  # GET /searches/1/edit
+  def edit
+  end
+
+  # POST /searches
+  def create
+    @search = Search.new(search_params)
+
+    if @search.save
+      redirect_to @search, notice: 'Search was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /searches/1
+  def update
+    if @search.update(search_params)
+      redirect_to @search, notice: 'Search was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /searches/1
+  def destroy
+    @search.destroy
+    redirect_to searches_url, notice: 'Search was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_search
+      @search = Search.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def search_params
+      params.fetch(:search, {})
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :searches
   root to: 'high_voltage/pages#show', id: 'home'
 end
